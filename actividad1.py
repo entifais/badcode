54de57dc643a214f94556e0066abb1969bae1106
jero98772 <jero98772@protonmail.com>
2022-08-26 10:07:33 -0500

    add explicacion

--- actividad1.py
+++ actividad1.py
@@ -1,7 +1,21 @@
-times=int(input())
-values={}
-for i in range(t):
-	key,value=input.split()
-	values[key]=values
+class node:
+		def __init__(self,frist,second):
+			self.frist=frist
+			self.second=second
+class tree:
+	def __init__(self,val):
+		self.key=val
+		self.child=[]
 
-string=input()
+def dfs(arr,node)
+
+times=int(input())
+while times: 
+	values={}
+	for i in range(t):
+		key,value=input.split()
+		values[key]=values
+	string=input()
+	if string=="":
+		print("")
+	times=int(input())
