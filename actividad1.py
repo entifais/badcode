9768a81d2782c009e11ae0e1675328723ce44fa4
jero98772 <jero98772@protonmail.com>
2022-08-28 22:25:52 -0500

    update code

--- actividad1.py
+++ actividad1.py
@@ -7,15 +7,23 @@ class tree:
 		self.key=val
 		self.child=[]
 
-def dfs(arr,node)
+def solve(values,oldstr,newstr=""):
+	for i in range(values):
+		ans=oldstr.find(i)
+		if ans!=-1:#found it
+			oldstr=oldstr.replace(i,"")
+			newstr+=values[i]
+			return solve(values,oldstr,newstr)
+		else:#parada?
 
 times=int(input())
-while times: 
+while times:# by default if is 0 stop
 	values={}
 	for i in range(t):
 		key,value=input.split()
 		values[key]=values
 	string=input()
-	if string=="":
-		print("")
+	solve(values,string,newstr="")
+	#if string=="":#the argortitm know if is empty return empty
+	#	print("")
 	times=int(input())
